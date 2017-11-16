f58cce820135e1a333d812f4079681549ac66a35
Giovanni Garzella <giovagarzella@gmail.com>
2017-11-16 15:29:02 +0100

    Eliminato da home-center codice commentato

--- page-courtesy.php
+++ page-courtesy.php
@@ -2,7 +2,7 @@
 
     <script>
     // Set the date we're counting down to
-    /*var countDownDate = new Date("Nov 30, 2017 01:00:00").getTime();
+    /*var countDownDate = new Date("Dec 15, 2017 08:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
@@ -38,7 +38,7 @@
             </div>
             <div id="intestazione">PARTE DI MEZZOGIORNO</div>
             <div id="sotto-intestazione">Presto online</div>
-            <!--<div id="counter"></div>-->
+            <div id="counter"></div>
         </div>
     </div>
 <?php get_footer(); ?>
